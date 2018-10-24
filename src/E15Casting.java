38e1d9b95ce9e73b3b4331490abe8832d1a954bb
Michelle Strout <mstrout@cs.arizona.edu>
2018-10-24 13:46:22 -0700

    Maze example is working.

--- src/E15Casting.java
+++ src/E15Casting.java
@@ -8,15 +8,15 @@ public class E15Casting {
     public static void main(String[] args) {
         Precipitation var = new Rain(2);
         Snow s1 = (Snow) new Precipitation(4);
-        Snow s2 = (Snow) new Rain(4);
-        Snow s3 = new Precipitation(0.5);
+        // Snow s2 = (Snow) new Rain(4);
+        // Snow s3 = new Precipitation(0.5);
         Precipitation r1 = new Rain(1);
         Rain r2 = (Rain) r1;
 
         Set<Integer> set1 = new HashSet<>();
-        int i1 = set1.first();
+        // int i1 = set1.first();
         Set<Integer> set2 = new TreeSet<>();
-        int i2 = set2.first();
+        // int i2 = set2.first();
         SortedSet<Integer> set3 = new TreeSet<>();
         int i3 = set3.first();
     }
