20562107d77a51bba7085b918db58ca9e496b84e
Michelle Strout <mstrout@cs.arizona.edu>
2018-11-27 17:14:00 -0700

    Lambda expressions to reuse code example.

--- src/E26LambdaForReuse.java
+++ src/E26LambdaForReuse.java
@@ -9,33 +9,29 @@ public class E26LambdaForReuse {
         capMinZero(data);
         // how could we print all of the elements in the array?
         
-        System.out.println("======= data2");
+        System.out.println("\n======= data2");
         int[] data2 = { 1, 2, 3, 4, 5, 23, 1, 45, 5 };
-        processArray(data2, ??);
-        processArray(data2, ??);
-        processArray(data2, ??);
+        processArray(data2, (int x)->{ int y = x+1; 
+           System.out.print(" " + y); return y; });
+        processArray(data2, (x)->x*2);
+        processArray(data2, (x) -> (x < 0) ? 0 : x);
     }
-
     public static void processArray(int[] data, ElemLambda func) {
         for (int i = 0; i < data.length; i++) {
             data[i] = func.process(data[i]);
         }
     }
-
-
     public static void addOne(int[] data) {
         for (int i = 0; i < data.length; i++) {
             data[i] = data[i] + 1;
             System.out.print(" " + data[i]);
         }
     }
-
     public static void timesTwo(int[] data) {
         for (int i = 0; i < data.length; i++) {
             data[i] = data[i] * 2;
         }
     }
-
     public static void capMinZero(int[] data) {
         for (int i = 0; i < data.length; i++) {
             if (data[i] < 0) {
---
+++ src/ElemLambda.java
@@ -0,0 +1,4 @@
+
+public interface ElemLambda {
+    public int process(int p);
+}
