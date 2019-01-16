58401f80ae6c749e93cf139bfd710c5b6509c165
Tyler Conklin <tylerconklin@dhcp-10-134-226-58.uawifi.arizona.edu>
2019-01-16 11:45:05 -0700

    Add commandLine example.

---
+++ src/E02CommandLine.java
@@ -0,0 +1,12 @@
+
+public class E02CommandLine {
+
+    public static void main(String[] whatever) {
+        System.out.println("Command line arguments");
+        for (int i = 0; i < whatever.length; i++) {
+            System.out.println("args[" + i + "] = " + whatever[i]);
+        }
+
+    }
+
+}
