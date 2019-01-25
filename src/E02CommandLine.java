6c615b80a937ba32bc79fee33202713e7af89ad3
Tyler Conklin <tylerconklin@dhcp-10-134-215-250.uawifi.arizona.edu>
2019-01-25 09:55:32 -0700

    Add comments to CommandLine example.

--- src/E02CommandLine.java
+++ src/E02CommandLine.java
@@ -1,6 +1,14 @@
 
 public class E02CommandLine {
 
+    /*
+     * Example to print out command line arguments passed to the program.
+     * In order to pass command line arguments to the program, select the
+     * program you want to run, click 'Run' on the top menu, then 'Run
+     * Configurations' then click on the arguments tab, and in the space
+     * for 'program arguments' write out your list of arguments. Each
+     * argument should be separated by a space.
+     */
     public static void main(String[] whatever) {
         System.out.println("Command line arguments");
         for (int i = 0; i < whatever.length; i++) {
