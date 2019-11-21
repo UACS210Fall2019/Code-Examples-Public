8e0a66df4a5d462c14d621e37dd77843e005a081
Tyler Conklin <tylerconklin@dhcp-10-142-223-202.uawifi.arizona.edu>
2019-11-21 15:26:53 -0700

    Add E27 Examples.

---
+++ src/E27DiceRoll.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class E27DiceRoll {
+
+    public static void main(String[] args) {
+        diceRoll(2);
+    }
+
+    private static void diceRoll(int numDie) {
+        diceRollHelper(numDie, new ArrayList<Integer>());
+    }
+
+    private static void diceRollHelper(int numDie, List<Integer> soFar) {
+        // Base Case
+        if (numDie == 0) {
+            System.out.println(soFar);
+        } else {
+            // Choose
+            for (int i = 1; i <= 6; i++) {
+                soFar.add(i);
+                // Explore
+                diceRollHelper(numDie - 1, soFar);
+                // Unchoose
+                // Try removing the below line and see what happens.
+                // Can you understand why?
+                soFar.remove(soFar.size() - 1);
+            }
+        }
+    }
+
+
+}
---
+++ src/E27DiceSum.java
@@ -0,0 +1,7 @@
+public class E27DiceSum {
+
+    public static void main(String[] args) {
+    }
+
+
+}
